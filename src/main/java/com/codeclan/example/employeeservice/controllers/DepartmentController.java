cc854884a3d27175eb69f2810eb688c3c48f3904
Benjamin Smith <bsmith@cabbage.org.uk>
2023-01-31 11:39:05 +0000

    Route to get employees in department.

--- src/main/java/com/codeclan/example/employeeservice/controllers/DepartmentController.java
+++ src/main/java/com/codeclan/example/employeeservice/controllers/DepartmentController.java
@@ -1,12 +1,14 @@
 package com.codeclan.example.employeeservice.controllers;
 
 import com.codeclan.example.employeeservice.models.Department;
+import com.codeclan.example.employeeservice.models.Employee;
 import com.codeclan.example.employeeservice.repositories.DepartmentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -23,5 +25,15 @@ public class DepartmentController {
     @GetMapping(value = "/departments/{id}")
     public Optional<Department> findOneDepartments(@PathVariable Long id) {
         return departmentRepository.findById(id);
+    }
+
+    @GetMapping(value = "/departments/{id}/employees")
+    public List<Employee> findEmployeesInDepartment(@PathVariable Long id) {
+        Optional<Department> department = departmentRepository.findById(id);
+        if (department.isPresent()) {
+            return department.get().getEmployees();
+        } else {
+            return new ArrayList<>();
+        }
     }
 }
